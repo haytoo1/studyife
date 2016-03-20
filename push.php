6cd7121832a5c88054da747b61b7fc3ccf9816ae
涂鸿 <466594257@qq.com>
2016-03-21 00:28:55 +0800

    测试 webhook

--- push.php
+++ push.php
@@ -8,3 +8,4 @@
 $shell = 'cd /home/www/studyife ; git pull pull https://github.com/haytoo1/studyife.git && echo `date` >> push.log';
 $val = system($shell,$return);
 file_put_contents('/log/e.log',$val.$return);
+echo $val.$return;
