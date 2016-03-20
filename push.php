b79f0ac2057688e0d2f8d2388d2000514f758cc7
涂鸿 <466594257@qq.com>
2016-03-21 01:09:21 +0800

    测试 webhook

--- push.php
+++ push.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hello
- * Date: 16/3/21
- * Time: 00:22
- */
 $shell = 'cd /home/www/studyife ; git pull pull https://github.com/haytoo1/studyife.git && echo `date` >> push.log';
 $val = system($shell,$return);
 file_put_contents('/log/e.log',$val.$return);
